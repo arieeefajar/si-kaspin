b9e95fdf7a123c376c7f481425e83e1bfe16e304
arieeefajar <ariefajar739@gmail.com>
2024-05-07 09:35:46 +0700

    feat: add alert in operator menu

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Pacakge custom
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+
     ],
 
     /*
