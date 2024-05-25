cdb2500a94be02c966d48bcbf8e758a98b675229
arieeefajar <ariefajar739@gmail.com>
2024-05-25 22:50:44 +0700

    feat: function retur penjualan

--- app/Http/Controllers/ReturPenjualanController.php
+++ app/Http/Controllers/ReturPenjualanController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetailReturPenjualan;
 use App\Models\Produk;
+use App\Models\ReturPenjualan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ReturPenjualanController extends Controller
 {
@@ -15,6 +19,62 @@ class ReturPenjualanController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+        $customMessagae = [
+            'kode_pelanggan.required' => 'Harap pilih nama pelanggan',
+            'total.required' => 'Harap masukkan total',
+            'bayar.required' => 'Harap masukkan bayar',
+            'kembalian.required' => 'Harap masukkan kembalian',
+        ];
+
+        $validator  = Validator::make($request->all(), [
+            'kode_pelanggan' => 'required',
+            'total' => 'required',
+            'bayar' => 'required',
+            'kembalian' => 'required',
+        ], $customMessagae);
+
+        if ($validator->fails()) {
+            alert()->error('Gagal', $validator->errors()->all()[0]);
+            return redirect()->back()->withInput();
+        }
+
+        // generate kode
+        $getKode = ReturPenjualan::latest()->first();
+        $kode = "RTP-";
+        if ($getKode == null) {
+            $kode .= "00001";
+        } else {
+            $kode .= sprintf("%05d", $getKode->id + 1);
+        }
+
+        $retur = new ReturPenjualan();
+        $retur->kode_retur = $kode;
+        $retur->kode_operator = Auth::user()->id;
+        $retur->kode_pelanggan = $request->kode_pelanggan;
+        $retur->total = $request->total;
+        $retur->bayar = $request->bayar;
+        $retur->kembalian = $request->kembalian;
+
+        $details = [];
+        foreach ($request->kode_produk as $index => $kode_produk) {
+            $detail = new DetailReturPenjualan();
+            $detail->kode_retur = $kode;
+            $detail->kode_produk = $kode_produk;
+            $detail->jumlah = $request->jumlah[$index];
+            $detail->subtotal = $request->subtotal[$index];
+            $details[] = $detail;
+        }
+
+        try {
+            $retur->save();
+            foreach ($details as $detail) {
+                $detail->save();
+            }
+            alert()->success('Berhasil', 'Data retur penjualan berhasil ditambahkan');
+            return redirect()->back();
+        } catch (\Throwable $th) {
+            alert()->error('Gagal', 'Data retur penjualan gagal ditambahkan');
+            return redirect()->back()->withInput();
+        }
     }
 }
